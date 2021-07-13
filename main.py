3b57fbd31533b0d66d7e7e9a0cc19665b8aa8558
merrick <merrickhare@outlook.com>
2021-07-12 18:08:27 -0600

    commit on main

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from tkinter import Tk
 import run
 
 w = tkinter.Tk()
-wn = run.win(w, "400x400", "Convert MP$ to FLV", "icon.png")
+wn = run.win(w, "400x400", "Convert MP4 to FLV", "icon.png")
 w.mainloop()
 
 
